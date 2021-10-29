c82dac137b3c196e4508c14315322580d581160d
Miroslav <vasilevm4@gmail.com>
2021-10-29 13:35:40 +0300

    Update

---
+++ 04.Entity Relations/P01_StudentSystem/Data/Models/StudentCourse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class StudentCourse
+    {
+        public int StudentId { get; set; }
+        public Student Students { get; set; }
+
+        public int CourseId { get; set; }
+        public Course Courses { get; set; }
+    }
+}
