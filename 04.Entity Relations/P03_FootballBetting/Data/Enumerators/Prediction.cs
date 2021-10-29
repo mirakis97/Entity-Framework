cca442631dc3e5c918d10aca74ec2fba0225c136
Miroslav <vasilevm4@gmail.com>
2021-10-29 23:07:45 +0300

    Update

---
+++ 04.Entity Relations/P03_FootballBetting/Data/Enumerators/Prediction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Enumerators
+{
+    public enum Prediction
+    {
+        Win = 1,
+        Lose = 2,
+        Draw = 3
+    }
+}
---
+++ 04.Entity Relations/P03_FootballBetting/Data/Models/Color.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Models
+{
+    public class Color
+    {
+        public Color()
+        {
+            this.PrimaryKitTeams = new HashSet<Team>();
+            this.SecondaryKitTeams = new HashSet<Team>();
+        }
+        [Key]
+        public int ColorId { get; set; }
+        public string Name { get; set; }
+        [NotMapped]
+        public ICollection<Team> PrimaryKitTeams { get; set; }
+        [NotMapped]
+        public ICollection<Team> SecondaryKitTeams { get; set; }
+
+    }
+}
